2e67e2b51f57cf4eef01b116fdde56c2b64ec500
Wyatt Chamberlain <wchambe8@gmu.edu>
2026-04-23 19:53:59 -0400

    Forgot to make a new branch to continue on.

--- src/Characters/Gambler.java
+++ src/Characters/Gambler.java
@@ -3,7 +3,10 @@ public class Gambler extends NPCharacter{
     //This character will play a dice game with the player. Win = gold, lose = gambler takes
     //- an item from the player
 
-
+    //Constructor
+    public Gambler(String name, int hp, int mp, int gp, String[] greetings, String[] goodbyes, String[] dialogueOptions){
+        super(name, hp, mp, gp, greetings, goodbyes, dialogueOptions);
+    }
 
 
 }//End of class
